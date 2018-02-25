be7662f4b345ccb4679b04326b8f290769810e38
李伟鹏 <674107287@qq.com>
2018-02-25 11:47:40 +0800

    尝试制作主文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { createStore, combineReducers, Store } from 'redux'
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { injectDispatch } from './services/redux-xstore';
+import { makeRootStore } from './services/redux-xstore';
 import './index.css';
 
 // 引入组件
 import App, { store as appStore } from './containers/App';
 import Home, { store as homeStore } from './containers/Home';
-
-function makeRootStore(storeTree: any): Store<any>{
-
-    // TODO: 遍历 storeTree 生成 rootReducer
-    // 并移入 pastate 内部
-    const rootReducer = combineReducers({
-        app: appStore.getReduxReducer(),
-        home: homeStore.getReduxReducer()
-    })
-
-    let reduxDevTools = window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__']()
-    let rootStore = createStore(rootReducer, reduxDevTools)
-    injectDispatch(rootStore, [appStore, homeStore])
-    return rootStore;
-}
 
 let store = makeRootStore({
     app: appStore,
